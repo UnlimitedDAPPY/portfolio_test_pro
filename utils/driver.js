aec325a541a89397e84192d5c521c5e2e4da825f
Oladapo Olawunmi <dapoasubiojo@gmail.com>
2026-05-25 12:22:26 +0100

    test for CI

--- utils/driver.js
+++ utils/driver.js
@@ -1,9 +1,34 @@
+// const { Builder } = require("selenium-webdriver");
+// const chrome = require("selenium-webdriver/chrome");
+
+// async function buildDriver() {
+//   let options = new chrome.Options();
+//   options.addArguments("--start-maximized");
+
+//   return new Builder()
+//     .forBrowser("chrome")
+//     .setChromeOptions(options)
+//     .build();
+// }
+
+// module.exports = buildDriver;
+
+
 const { Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
 async function buildDriver() {
   let options = new chrome.Options();
-  options.addArguments("--start-maximized");
+
+  // Detect CI environment (GitHub Actions)
+  if (process.env.CI) {
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--window-size=1920,1080");
+  } else {
+    options.addArguments("--start-maximized");
+  }
 
   return new Builder()
     .forBrowser("chrome")
